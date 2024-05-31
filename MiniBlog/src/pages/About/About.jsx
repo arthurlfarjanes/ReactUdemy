d560094d4f9916e07f2bdafd182087b4c6b84dd8
Arthur Lanzilotti Farjanes <arthurlfarjanes@gmail.com>
2024-05-31 12:52:32 -0300

    Projeto finalizado

--- MiniBlog/src/pages/About/About.jsx
+++ MiniBlog/src/pages/About/About.jsx
@@ -9,7 +9,7 @@ function About() {
     <div className={styles.about}>
       <h2>Sobre o Mini<span>Blog</span></h2>
       <p>Este projeto consiste em um blog feito com React no front-end e Firebase no back-end.</p>
-      <Link to="/post/create" className="btn">Criar Post</Link>
+      <Link to="/posts/create" className="btn">Criar Post</Link>
     </div>
   )
 }
